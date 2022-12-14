b1c407fcd8549ab17de7c4be5c16638df675d4a5
BaoSkyFall <39408162+BaoSkyFall@users.noreply.github.com>
2022-12-15 00:34:23 +0700

    CAX

--- api/index.js
+++ api/index.js
@@ -9,6 +9,8 @@ const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 const stripeRoute = require("./routes/stripe");
+const categoryRoute = require("./routes/category");
+
 const cors = require("cors");
 
 
@@ -27,6 +29,8 @@ app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
+app.use("/api/categories", categoryRoute);
+
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
